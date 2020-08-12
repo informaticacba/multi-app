bfc4ef83a03adf756664cb6fea6e950105f17f5b
Jorge Eduardo <corpjorge@hotmail.com>
2020-08-12 16:13:49 -0500

    Santum

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -6,7 +6,10 @@ use Illuminate\Http\Request;
 
 class ViewController extends Controller
 {
-    public function Work(){
-        return true; 
+    public function sesion(Request $request){
+        if(!$token =  auth()->attempt($request->only('email', 'password'))){
+            return response(null, 401);
+        }
+        return response()->json(compact('token'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {  return view('index'); });
-Route::get('/home', function () {  return view('index'); });
-Route::get('/tabla', function () {  return view('index'); });
+ 
+Route::post('sesion', 'ViewController@sesion');
 
 Auth::routes();
